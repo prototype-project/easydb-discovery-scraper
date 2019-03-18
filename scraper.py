629c3237f51c8753a82d607e2ff741ea8adc2daa
Daniel Faderski <daniel.faderski@allegro.pl>
2019-03-19 00:12:57 +0100

    Scraper

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ from kazoo.client import KazooClient
 ZOOKEEPER_HOSTS = '127.0.0.1:2181'
 SERVICE_NAME = 'Easydb'
 LOAD_BALANCERS_STR = '127.0.0.1:8081'
+PROMETHEUS_TARGETS_FILE_PATH = '/etc/prometheus/targets/targets.json'
 SLEEP_TIME_SECONDS = 1
 
 try:
@@ -27,6 +28,11 @@ except:
     pass
 
 LOAD_BALANCERS = LOAD_BALANCERS_STR.split(";")
+
+try:
+    PROMETHEUS_TARGETS_FILE_PATH = os.environ['PROMETHEUS_TARGETS_FILE_PATH']
+except:
+    pass
 
 try:
     SLEEP_TIME_SECONDS = int(os.environ['SLEEP_TIME_SECONDS'])
@@ -63,30 +69,45 @@ def parse_instance(instance_data: bytes):
     data_as_dict = json.loads(instance_data)
     return "%s:%s" % (data_as_dict['address'], data_as_dict['port'])
 
+def notify_listeners(services):
+    return all([send_instances_to_load_balancer(services), update_prometheus_targets(services)])
+
 def send_instances_to_load_balancer(services):
     data = ""
-    success = True
     for s in services:
         data += "server %s;" % s
 
     for load_balancer in LOAD_BALANCERS:
         response = requests.post("http://%s/upstream/backend" % load_balancer, data)
         if response.status_code >= 300:
-            logger.warning("Failed to pass services ips to load balancer %s. Status code %s", load_balancer, response.status_code)
-            success = False
+            raise RuntimeError("Failed to pass services ips to load balancer %s. Status code %s", load_balancer, response.status_code)
 
-    return success
+
+def update_prometheus_targets(services):
+    data = {
+        "labels": {
+            "alias": "Easydb"
+        },
+        "targets": [
+        ]
+    }
+    for s in services:
+        data['targets'].append(s)
+
+    with open(PROMETHEUS_TARGETS_FILE_PATH, 'w') as outfile:
+        json.dump(data, outfile)
 
 def run():
     previous_backends = []
-    load_balancers_notified_correctly = False
+    changes_listeners_notified_correctly = False
     while True:
         try:
             time.sleep(SLEEP_TIME_SECONDS)
             current_backends = read_services()
-            if current_backends != previous_backends or not load_balancers_notified_correctly:
+            if current_backends != previous_backends or not changes_listeners_notified_correctly:
                 previous_backends = current_backends
-                load_balancers_notified_correctly = send_instances_to_load_balancer(current_backends)
+                notify_listeners(current_backends)
+                changes_listeners_notified_correctly = True
                 logger.info("Found active backends: [%s]" % ", ".join(current_backends))
         except Exception as e:
             logger.exception("Failed to perform discovery scrapping %s", e)
